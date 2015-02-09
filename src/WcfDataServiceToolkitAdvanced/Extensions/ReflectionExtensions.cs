f6d371e7ee5cc0c910a8b914b9421300c3ca0951
khoale <froghv@gmail.com>
2015-02-09 13:55:47 +0700

    Refactor code

---
+++ src/WcfDataServiceToolkitAdvanced/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace WcfDataServiceToolkitAdvanced.Extensions
+{
+    using System;
+
+    public static class ReflectionExtensions
+    {
+        public static bool IsImplementGenericType(this Type type, Type genericType)
+        {
+            return
+                type.BaseType != null && type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == genericType;
+        }
+
+        public static bool IsConcreteType(this Type type)
+        {
+            return type.IsClass && !type.IsAbstract && !type.IsInterface;
+        }
+    }
+}
