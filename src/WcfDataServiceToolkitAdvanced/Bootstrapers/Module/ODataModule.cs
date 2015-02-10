3cc95fad0704aa6e1cc9b007ae1b3e0edb58fb98
khoale <froghv@gmail.com>
2015-02-10 14:44:59 +0700

    Refactor code

--- src/WcfDataServiceToolkitAdvanced/Bootstrapers/Module/ODataModule.cs
+++ src/WcfDataServiceToolkitAdvanced/Bootstrapers/Module/ODataModule.cs
@@ -44,64 +44,70 @@
         protected override void OnStart()
         {
             AutofacHostFactory.Container = this.rootScope;
-            this.RegisterRouteTable();
-            this.RegisterRepositories();
+            ODataServiceRouting.RegisterRouteTable(this.serviceHostFactory);
+            RepositoryRegistration.RegisterRepositories(this.repositoryFactory);
         }
 
-        private void RegisterRepositories()
+        private static class ODataServiceRouting
         {
-            //// Register resource manualy
-            // this.repositoryFactory
-            //     .RegisterResource<PersonDto, PeopleRepository>();
-
-            // Register using reflection
-            var repositoryTypes = this.GetAllRepositoryTypes();
-            Debug.Assert(repositoryTypes.Length > 0, "repositoryTypes.Length > 0");
-            var registerSourceMethod = typeof(RepositoryFactory).GetMethod("RegisterResource");
-
-            foreach (var repositoryType in repositoryTypes)
+            public static void RegisterRouteTable(ODataServiceHostFactory serviceHostFactory)
             {
-                Debug.Assert(repositoryType.BaseType != null, "repositoryType.BaseType != null");
-                var resourceType = repositoryType.BaseType.GetGenericArguments()[0];
-                var genericRegisterSourceMethod =
-                    registerSourceMethod.MakeGenericMethod(resourceType, repositoryType);
+                var serviceTypes = GetAllServiceTypes();
+                Debug.Assert(serviceTypes.Length > 0, "serviceTypes.Length > 0");
 
-                genericRegisterSourceMethod.Invoke(this.repositoryFactory, null);
+                foreach (var serviceType in serviceTypes)
+                {
+                    var serviceName = GetServiceName(serviceType);
+                    var prefix = string.Format("api/{0}", serviceName).ToLower();
+                    RouteTable.Routes.Add(new ServiceRoute(prefix, serviceHostFactory, serviceType));
+                }
+            }
+
+            private static Type[] GetAllServiceTypes()
+            {
+                return typeof(PersonDataService).Assembly.GetTypes()
+                    .Where(x => x.IsConcreteType())
+                    .Where(x => x.IsImplementGenericType(typeof(ODataService<>)))
+                    .ToArray();
+            }
+
+            private static string GetServiceName(Type serviceType)
+            {
+                return serviceType.Name.Replace("DataService", string.Empty);
             }
         }
 
-        private void RegisterRouteTable()
+        private static class RepositoryRegistration
         {
-            var serviceTypes = this.GetAllServiceTypes();
-            Debug.Assert(serviceTypes.Length > 0, "serviceTypes.Length > 0");
-
-            foreach (var serviceType in serviceTypes)
+            public static void RegisterRepositories(RepositoryFactory repositoryFactory)
             {
-                var serviceName = this.GetServiceName(serviceType);
-                var prefix = string.Format("api/{0}", serviceName).ToLower();
-                RouteTable.Routes.Add(new ServiceRoute(prefix, this.serviceHostFactory, serviceType));
+                //// Register resource manualy
+                // this.repositoryFactory
+                //     .RegisterResource<PersonDto, PeopleRepository>();
+
+                // Register using reflection
+                var repositoryTypes = GetAllRepositoryTypes();
+                Debug.Assert(repositoryTypes.Length > 0, "repositoryTypes.Length > 0");
+                var registerSourceMethod = typeof(RepositoryFactory).GetMethod("RegisterResource");
+
+                foreach (var repositoryType in repositoryTypes)
+                {
+                    Debug.Assert(repositoryType.BaseType != null, "repositoryType.BaseType != null");
+                    var resourceType = repositoryType.BaseType.GetGenericArguments()[0];
+                    var genericRegisterSourceMethod =
+                        registerSourceMethod.MakeGenericMethod(resourceType, repositoryType);
+
+                    genericRegisterSourceMethod.Invoke(repositoryFactory, null);
+                }
             }
-        }
 
-        private Type[] GetAllRepositoryTypes()
-        {
-            return typeof(RepositoryBase<,>).Assembly.GetTypes()
-                .Where(x => x.IsConcreteType())
-                .Where(x => x.IsImplementGenericType(typeof(RepositoryBase<,>)))
-                .ToArray();
-        }
-
-        private Type[] GetAllServiceTypes()
-        {
-            return typeof(PersonDataService).Assembly.GetTypes()
-                .Where(x => x.IsConcreteType())
-                .Where(x => x.IsImplementGenericType(typeof(ODataService<>)))
-                .ToArray();
-        }
-
-        private string GetServiceName(Type serviceType)
-        {
-            return serviceType.Name.Replace("DataService", string.Empty);
+            private static Type[] GetAllRepositoryTypes()
+            {
+                return typeof(RepositoryBase<,>).Assembly.GetTypes()
+                    .Where(x => x.IsConcreteType())
+                    .Where(x => x.IsImplementGenericType(typeof(RepositoryBase<,>)))
+                    .ToArray();
+            }
         }
     }
 }
